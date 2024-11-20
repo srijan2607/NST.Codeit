245289710bfbd6b95e52b790704e781c5901f699
srijan2607 <srijan.mart@gmail.com>
2024-11-20 12:05:23 +0530

    NST-Update

---
+++ PSP.codeit/PSP-class/set.py
@@ -0,0 +1,4 @@
+set_1 = {4,5,6,7,8,9,0}
+set_2 = {1,2,3,4,5}
+set_1.update(set_2)
+print(set_1)
---
+++ PSP.codeit/PSP.Lab/dict.py
@@ -0,0 +1,6 @@
+dict = {}
+for i in range (3):
+    letter,number = input().split()
+    dict[letter] = number
+print(dict)
+print(dict.keys())
